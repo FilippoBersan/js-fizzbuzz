77f86dc539c1909462473d661ee0c2634a1019b9
FilippoBersan <filippobersan99@gmail.com>
2023-11-08 17:11:51 +0100

    fix: fix js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+const elementUl = document.querySelector('ul.list');
 
 let contatoreInizio = 0;
 let contatoreFine = 100;
