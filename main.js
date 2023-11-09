f6c8e0f6bc97ce52d2c93b6d964d11982b428f97
FilippoBersan <filippobersan99@gmail.com>
2023-11-09 10:12:13 +0100

    fix: valore contatore 1 invece che zero

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 const elementUl = document.querySelector('ul.list');
 
-let contatoreInizio = 0;
+let contatoreInizio = 1;
 let contatoreFine = 100;
 
 for (let i = contatoreInizio; i <= contatoreFine; i++) {
