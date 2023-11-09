0fa0c05a95fc280202caf965f1cbf832726d1ac1
FilippoBersan <filippobersan99@gmail.com>
2023-11-09 10:42:56 +0100

    fix: stampo nella pagina con append

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 
 // Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
 const elementUl = document.querySelector('ul.list');
+console.log(elementUl);
 
 let contatoreInizio = 1;
 let contatoreFine = 100;
@@ -14,21 +15,24 @@ for (let i = contatoreInizio; i <= contatoreFine; i++) {
 
   if (i % 3 === 0) {
     console.log(i, 'Fizz');
+    li.append('Fizz');
   }
   // multipli di 5/
   else if (i % 5 === 0) {
     console.log(i, 'Buzz');
+    li.append('Buzz');
   }
   // numeri 0 100/
   else if (i % 1 === 0) {
-    console.log('', i);
+    console.log(i);
+    li.append(i);
   }
   // multipli sia di 3 che di 5/
 
   if (i % 15 === 0) {
     console.log(i, 'FizzBuzz');
+    li.append('FizzBuzz');
   }
 
   elementUl.append(li);
-  li.append();
 }
